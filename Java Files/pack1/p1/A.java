87c488ca4c0a75bf0f873abeaf8e6dbcc04e5057
JollyRocketts <jollyaryan7@gmail.com>
2024-06-20 10:42:33 +0530

    More files

---
+++ Java Files/pack1/p1/A.java
@@ -0,0 +1,9 @@
+package pack1.p1;
+
+public class A {
+    private int data = 100;
+    public void display()
+    {
+        System.out.println("NPTEL " + data);
+    }
+}
---
+++ Java Files/pack2/p2/B.java
@@ -0,0 +1,9 @@
+package pack2.p2;
+
+class B {
+    public static void main(String args[]){
+        //A obj = new A();   //Compile Time Error
+        //obj.display();         //Compile Time Error
+        System.out.println("hello");
+    }
+}
